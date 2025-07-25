09c67d048610a4673f916e15840c2d89b3576430
Nathan <visualmusicalminds@gmail.com>
2025-07-25 00:55:31 -0400

    Update state.js

--- js/state.js
+++ js/state.js
@@ -42,6 +42,7 @@ export const appState = {
   progressionA: ['', '', '', ''],
   rhythmBoxesA: Array(10).fill(false),
   seventhA: [false, false, false, false],
+  sixthA: [false, false, false, false],
   secondA: [false, false, false, false],
   fourthA: [false, false, false, false],
   susA: [false, false, false, false],
@@ -51,6 +52,7 @@ export const appState = {
   splitChordActiveA: [false, false, false, false],
   splitChordValueA: ['', '', '', ''],
   splitSeventhA: [false, false, false, false],
+  splitSixthA: [false, false, false, false],
   splitSecondA: [false, false, false, false],
   splitFourthA: [false, false, false, false],
   splitSusA: [false, false, false, false],
@@ -62,6 +64,7 @@ export const appState = {
   progressionB: ['', '', '', ''],
   rhythmBoxesB: Array(10).fill(false),
   seventhB: [false, false, false, false],
+  sixthB: [false, false, false, false],
   secondB: [false, false, false, false],
   fourthB: [false, false, false, false],
   susB: [false, false, false, false],
@@ -71,6 +74,7 @@ export const appState = {
   splitChordActiveB: [false, false, false, false],
   splitChordValueB: ['', '', '', ''],
   splitSeventhB: [false, false, false, false],
+  splitSixthB: [false, false, false, false],
   splitSecondB: [false, false, false, false],
   splitFourthB: [false, false, false, false],
   splitSusB: [false, false, false, false],
@@ -82,6 +86,7 @@ export const appState = {
   progressionC: ['', '', '', ''],
   rhythmBoxesC: Array(10).fill(false),
   seventhC: [false, false, false, false],
+  sixthC: [false, false, false, false],
   secondC: [false, false, false, false],
   fourthC: [false, false, false, false],
   susC: [false, false, false, false],
@@ -91,6 +96,7 @@ export const appState = {
   splitChordActiveC: [false, false, false, false],
   splitChordValueC: ['', '', '', ''],
   splitSeventhC: [false, false, false, false],
+  splitSixthC: [false, false, false, false],
   splitSecondC: [false, false, false, false],
   splitFourthC: [false, false, false, false],
   splitSusC: [false, false, false, false],
@@ -102,6 +108,7 @@ export const appState = {
   progressionD: ['', '', '', ''],
   rhythmBoxesD: Array(10).fill(false),
   seventhD: [false, false, false, false],
+  sixthD: [false, false, false, false],
   secondD: [false, false, false, false],
   fourthD: [false, false, false, false],
   susD: [false, false, false, false],
@@ -111,6 +118,7 @@ export const appState = {
   splitChordActiveD: [false, false, false, false],
   splitChordValueD: ['', '', '', ''],
   splitSeventhD: [false, false, false, false],
+  splitSixthD: [false, false, false, false],
   splitSecondD: [false, false, false, false],
   splitFourthD: [false, false, false, false],
   splitSusD: [false, false, false, false],
@@ -124,10 +132,10 @@ export const appState = {
 
 export function getProgressionData(progLetter) {
   switch(progLetter) {
-    case 'A': return { p: appState.progressionA, r: appState.rhythmBoxesA, s7: appState.seventhA, s2: appState.secondA, s4: appState.fourthA, sus: appState.susA, aug: appState.augA, maj7: appState.majSeventhA, m: appState.majorA, splitActive: appState.splitChordActiveA, splitVal: appState.splitChordValueA, splitS7: appState.splitSeventhA, splitS2: appState.splitSecondA, splitS4: appState.splitFourthA, splitSus: appState.splitSusA, splitAug: appState.splitAugA, splitMaj7: appState.splitMajSeventhA, splitM: appState.splitMajorA };
-    case 'B': return { p: appState.progressionB, r: appState.rhythmBoxesB, s7: appState.seventhB, s2: appState.secondB, s4: appState.fourthB, sus: appState.susB, aug: appState.augB, maj7: appState.majSeventhB, m: appState.majorB, splitActive: appState.splitChordActiveB, splitVal: appState.splitChordValueB, splitS7: appState.splitSeventhB, splitS2: appState.splitSecondB, splitS4: appState.splitFourthB, splitSus: appState.splitSusB, splitAug: appState.splitAugB, splitMaj7: appState.splitMajSeventhB, splitM: appState.splitMajorB };
-    case 'C': return { p: appState.progressionC, r: appState.rhythmBoxesC, s7: appState.seventhC, s2: appState.secondC, s4: appState.fourthC, sus: appState.susC, aug: appState.augC, maj7: appState.majSeventhC, m: appState.majorC, splitActive: appState.splitChordActiveC, splitVal: appState.splitChordValueC, splitS7: appState.splitSeventhC, splitS2: appState.splitSecondC, splitS4: appState.splitFourthC, splitSus: appState.splitSusC, splitAug: appState.splitAugC, splitMaj7: appState.splitMajSeventhC, splitM: appState.splitMajorC };
-    case 'D': return { p: appState.progressionD, r: appState.rhythmBoxesD, s7: appState.seventhD, s2: appState.secondD, s4: appState.fourthD, sus: appState.susD, aug: appState.augD, maj7: appState.majSeventhD, m: appState.majorD, splitActive: appState.splitChordActiveD, splitVal: appState.splitChordValueD, splitS7: appState.splitSeventhD, splitS2: appState.splitSecondD, splitS4: appState.splitFourthD, splitSus: appState.splitSusD, splitAug: appState.splitAugD, splitMaj7: appState.splitMajSeventhD, splitM: appState.splitMajorD };
+    case 'A': return { p: appState.progressionA, r: appState.rhythmBoxesA, s7: appState.seventhA, s6: appState.sixthA, s2: appState.secondA, s4: appState.fourthA, sus: appState.susA, aug: appState.augA, maj7: appState.majSeventhA, m: appState.majorA, splitActive: appState.splitChordActiveA, splitVal: appState.splitChordValueA, splitS7: appState.splitSeventhA, splitS6: appState.splitSixthA, splitS2: appState.splitSecondA, splitS4: appState.splitFourthA, splitSus: appState.splitSusA, splitAug: appState.splitAugA, splitMaj7: appState.splitMajSeventhA, splitM: appState.splitMajorA };
+    case 'B': return { p: appState.progressionB, r: appState.rhythmBoxesB, s7: appState.seventhB, s6: appState.sixthB, s2: appState.secondB, s4: appState.fourthB, sus: appState.susB, aug: appState.augB, maj7: appState.majSeventhB, m: appState.majorB, splitActive: appState.splitChordActiveB, splitVal: appState.splitChordValueB, splitS7: appState.splitSeventhB, splitS6: appState.splitSixthB, splitS2: appState.splitSecondB, splitS4: appState.splitFourthB, splitSus: appState.splitSusB, splitAug: appState.splitAugB, splitMaj7: appState.splitMajSeventhB, splitM: appState.splitMajorB };
+    case 'C': return { p: appState.progressionC, r: appState.rhythmBoxesC, s7: appState.seventhC, s6: appState.sixthC, s2: appState.secondC, s4: appState.fourthC, sus: appState.susC, aug: appState.augC, maj7: appState.majSeventhC, m: appState.majorC, splitActive: appState.splitChordActiveC, splitVal: appState.splitChordValueC, splitS7: appState.splitSeventhC, splitS6: appState.splitSixthC, splitS2: appState.splitSecondC, splitS4: appState.splitFourthC, splitSus: appState.splitSusC, splitAug: appState.splitAugC, splitMaj7: appState.splitMajSeventhC, splitM: appState.splitMajorC };
+    case 'D': return { p: appState.progressionD, r: appState.rhythmBoxesD, s7: appState.seventhD, s6: appState.sixthD, s2: appState.secondD, s4: appState.fourthD, sus: appState.susD, aug: appState.augD, maj7: appState.majSeventhD, m: appState.majorD, splitActive: appState.splitChordActiveD, splitVal: appState.splitChordValueD, splitS7: appState.splitSeventhD, splitS6: appState.splitSixthD, splitS2: appState.splitSecondD, splitS4: appState.splitFourthD, splitSus: appState.splitSusD, splitAug: appState.splitAugD, splitMaj7: appState.splitMajSeventhD, splitM: appState.splitMajorD };
     default: return null;
   }
 }
